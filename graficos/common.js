8e029ba022c35397aad627aedd0ebe4ec4b78dd7
Hillian Gabriel <106557047+UnkNownUserhx@users.noreply.github.com>
2024-09-25 10:56:12 -0300

    Common

--- graficos/common.js
+++ graficos/common.js
@@ -1,4 +1,12 @@
-const GETCSS = (variavel) => {
+const getCSS = (variavel) => {
     const BodyStyles = getComputedStyle(document.body)
     return BodyStyles.getPropertyValue(variavel)
 }
+
+const tickconfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickconfig}
