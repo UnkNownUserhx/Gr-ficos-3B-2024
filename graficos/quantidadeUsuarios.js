9e16b37718af151a47055c88e2e8cfd3036fff8a
Hillian Gabriel <106557047+UnkNownUserhx@users.noreply.github.com>
2024-09-25 11:09:05 -0300

    asdasaasdasd

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -15,5 +15,39 @@ async function QuantidadeUsuariosPorRede() {
             color: getCSS('--primary-color')
         }
     ]
-   }
-    
+}
+
+
+const laytout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+        text: 'Redes sociais com mais usuários',
+        x: 0,
+        font: {
+            color: getCSS('--primary-color'),
+            size: 30,
+            font: getCSS('--font')
+        }
+    },
+    xaxis: {
+        tickfont: tickConfig,
+        title: {
+            text: 'Nome das redes',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    },
+    yaxis: {
+        tickfont: tickConfig,
+        title: {
+            text: 'Bilhões de usuários ativos',
+            font: {
+                color: getCSS('--secondary-color')
+            }
+        }
+    }
+}
+
+
